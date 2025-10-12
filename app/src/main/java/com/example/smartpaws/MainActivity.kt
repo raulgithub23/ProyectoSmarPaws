2c5f67510bbdb103d3a698ba0dab4edcd445020a
Riskoder <g.vidal1g+1@gmail.com>
2025-10-12 20:33:40 -0300

    Se cambio el thema de material por el de la App para ocupar colores propios

--- app/src/main/java/com/example/smartpaws/MainActivity.kt
+++ app/src/main/java/com/example/smartpaws/MainActivity.kt
@@ -3,19 +3,12 @@ package com.example.smartpaws
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.example.smartpaws.navigation.AppNavGraph
-import com.example.smartpaws.ui.theme.theme.SMARTPAWSTheme
+import com.example.smartpaws.ui.theme.SMARTPAWSTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,7 +33,7 @@ Piensa en él como una “lona base” sobre la cual vas a pintar tu UI.
 @Composable // Indica que esta función dibuja UI
 fun AppRoot() { // Raíz de la app para separar responsabilidades
     val navController = rememberNavController() // Controlador de navegación
-    MaterialTheme { // Provee colores/tipografías Material 3
+    SMARTPAWSTheme(dynamicColor = false) { // Provee colores/tipografías Material 3
         Surface(color = MaterialTheme.colorScheme.background) { // Fondo general
             AppNavGraph(navController = navController) // Carga el NavHost + Scaffold + Drawer
         }
