64c35a2c6d9cfa3bdedd110f685466106c6b3c11
Raul <ra.fernandezduocuc.cl.com>
2025-10-15 20:33:44 -0300

    se crea el intermediario entre el ViewModel y el DAO

--- app/src/main/java/com/example/smartpaws/data/repository/UserRepository.kt
+++ app/src/main/java/com/example/smartpaws/data/repository/UserRepository.kt
@@ -1,4 +1,38 @@
 package com.example.smartpaws.data.repository
 
-class UserRepository {
+import com.example.smartpaws.data.local.user.UserDao
+import com.example.smartpaws.data.local.user.UserEntity
+
+class UserRepository(
+    private val userDao: UserDao
+){
+    //login
+    suspend fun login(email:String, pass: String): Result<UserEntity>{
+        val user = userDao.getByEmail(email)
+        return if(user != null && user.password == pass){
+            Result.success(user)
+        }
+        else{
+            Result.failure(IllegalArgumentException("Credenciales Inválidas"))
+        }
+    }
+
+    //register
+    suspend fun register(name:String, email: String, phone: String, pass: String): Result<Long>{
+        val exists = userDao.getByEmail(email) != null
+        if(exists){
+            return Result.failure(IllegalArgumentException("Correo ya existente"))
+        }
+        else{
+            val id = userDao.insert(
+                UserEntity(
+                    name = name,
+                    email =  email,
+                    phone = phone,
+                    password = pass
+                )
+            )
+            return Result.success(id)
+        }
+    }
 }
